46daedb32070a4a906ace97346f85b7dab7e7c90
Felippe Vieira <felippe_nvieira@hotmail.com>
2023-04-27 17:36:57 -0300

    routes, components, api, userContext

---
+++ src/Components/Login/LoginForm.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { TOKEN_POST, USER_GET } from '../../api';
+import useForm from '../../Hooks/useForm';
+import { UserContext } from '../../UserContext';
+import Button from '../Forms/Button';
+import Input from '../Forms/Input';
+
+const LoginForm = () => {
+  const username = useForm();
+  const password = useForm();
+
+  const { userLogin } = React.useContext(UserContext);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (username.validate() && password.validate()) {
+      userLogin(username.value, password.value);
+    }
+  }
+
+  return (
+    <section>
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Entrar</Button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
